74aa723a6a1c7b53bfc3c182bc1066bce3660c18
DESKTOP-LGTFG4Q\daichi morita <daichi.927.m@gmail.com>
2017-11-15 02:37:25 +0900

    リサイズ処理追加、色相変換法の是正

--- detectMan.cpp
+++ detectMan.cpp
@@ -13,60 +13,20 @@
 using namespace cv;
 using namespace std;
 
-//polar coordinate
-//struct Pol{
-//	double d;
-//	double theta;
-//};
-
-static void rgb2hsv(Mat &img, Mat &hsv) {
-	hsv = img.clone();
-
-	for (int i = 0; i < img.cols; i++) {
-		for (int j = 0; j < img.rows; j++) {
-			double r = img.at<Vec3b>(j, i)[0];
-			double g = img.at<Vec3b>(j, i)[1];
-			double b = img.at<Vec3b>(j, i)[2];
-
-			double MAX = max(max(r, g), b) / 255;
-			double MIN = min(min(r, g), b) / 255;
-
-			double h, s, v;
-			if (MIN == b) {
-				h = 60 * (g - r) / (MAX - MIN) + 60;
-			}
-			else if (MIN == r) {
-				h = 60 * (b - g) / (MAX - MIN) + 180;
-			}
-			else {
-				h = 60 * (r - b) / (MAX - MIN) + 300;
-			}
-			h = h * 255 / 359;
-			s = (MAX - MIN) / MAX * 255;
-			v = MAX * 255;
-
-			hsv.at<Vec3b>(j, i)[0] = (int)h % 256;
-			hsv.at<Vec3b>(j, i)[1] = (int)s;
-			hsv.at<Vec3b>(j, i)[2] = (int)v;
-		}
-	}
-}
-
 static void mask(Mat &gray, Mat &img) {
-	Mat hsv;
-	rgb2hsv(img, hsv);
-
-	//cvCvtColor(&img, &hsv, CV_RGB2HSV_FULL);
-	for (int i = 0; i < gray.cols; i++) {
-		for (int j = 0; j < gray.rows; j++) {
-			bool b = hsv.at<Vec3b>(j, i)[0] > 10;
-			b = b && hsv.at<Vec3b>(j, i)[0] < 100;
-			b = b && hsv.at<Vec3b>(j, i)[1] > 50;
-			gray.at<uchar>(j, i) = b ? 255 : 0;
+	IplImage iplImage = img;
+	IplImage *hsv = cvCreateImage(cvGetSize(&iplImage), IPL_DEPTH_8U, 3);
+	cvCvtColor(&iplImage, hsv, CV_RGB2HSV_FULL);
+	for (int y = 0; y < hsv->height; y++) {
+		for (int x = 0; x < hsv->width; x++) {
+			bool b = hsv->imageData[hsv->widthStep*y + x * 3] > 10;
+			b = b && hsv->imageData[hsv->widthStep*y + x * 3] < 100;
+			b = b && hsv->imageData[hsv->widthStep*y + x * 3 + 1] > 50;
+			gray.at<uchar>(y, x) = b ? 255 : 0;
 		}
 	}
 
-	hsv.release();
+	cvReleaseImage(&hsv);
 }
 
 static void detectShirt(Mat &img, vector<Rect> &rects) {
@@ -79,13 +39,9 @@ static void detectShirt(Mat &img, vector<Rect> &rects) {
 	IplImage iplImage = gray;
 	cvFindContours(&iplImage, storage, &contours, sizeof(CvContour));
 
-	//gray.release();
+	gray.release();
 
-	//for (int i = 0; i < contours->total; i++) {
 	for (; contours != 0; contours = contours->h_next) {
-		//Mat *approx = 0;
-		//bug, CvSeq cannot convert to CvArr
-		//cvConvexHull2(&contours[i], approx);
 		Rect rect = cvBoundingRect(contours, 1);
 		auto x = rect;
 		if (!(x.width / x.height < 2) || !(x.height / x.width > 0.5)) {
@@ -93,15 +49,7 @@ static void detectShirt(Mat &img, vector<Rect> &rects) {
 		}
 	}
 
-	/*for (auto it = rects.end(); it != rects.begin(); it--) {
-		auto x = *it;
-		if ((x.width / x.height < 2)&(x.height / x.width > 0.5)) {
-			rects.erase(it);
-		}
-	}*/
-
 	cvReleaseMemStorage(&storage);
-
 }
 
 double calcDist(double r, double rmax) {
@@ -113,6 +61,12 @@ double calcDist(double r, double rmax) {
 }
 
 string detectMan(Mat &img, double oldD, double oldTheta) {
+	IplImage iplImage = img;
+	IplImage *resized = cvCreateImage(cvSize(iplImage.width / 5, iplImage.height / 5), IPL_DEPTH_8U, 3);
+	cvResize(&iplImage, resized);
+
+	img.release();
+	img = cvarrToMat(resized);
 
 	vector<Rect> rects;
 	detectShirt(img, rects);
@@ -137,6 +91,8 @@ string detectMan(Mat &img, double oldD, double oldTheta) {
 		r = sqrt(pow(x, 2) + pow(y, 2));
 		theta = atan2(x, y);
 	}
+
+	cvReleaseImage(&resized);
 	img.release();
 
 	int d = calcDist(r, w);
